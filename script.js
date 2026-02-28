5c9aa014f00e4c82dd48d0da5e9171858cda0f8f
sudo_make_coffee <101630050+ManaliSelmokar@users.noreply.github.com>
2026-03-01 02:05:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ if (container) {
     container.appendChild(sparkle);
   }
 }
+
+function blowCandles() {
+  const flames = document.querySelectorAll('.flame');
+
+  flames.forEach(flame => {
+    const smoke = document.createElement('div');
+    smoke.classList.add('smoke');
+    flame.parentElement.appendChild(smoke);
+
+    flame.style.display = "none";
+  });
+
+  setTimeout(() => {
+    alert("Wish made ✨");
+  }, 1500);
+}
